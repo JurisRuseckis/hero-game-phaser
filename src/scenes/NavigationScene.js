0713ef86b0fd743ca647eb44035d79ce1b94316f
Juris Ruseckis <juris.ruseckis@catchsmart.com>
2021-05-08 00:15:33 +0300

    refactor navigation to styles

--- src/scenes/NavigationScene.js
+++ src/scenes/NavigationScene.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import {cfg} from "../cfg";
+import {styles} from "../styles";
 
 export class NavigationScene extends Phaser.Scene
 {
@@ -17,32 +18,24 @@ export class NavigationScene extends Phaser.Scene
 
     create ()
     {
-        const { width, height } = this.scale;
+        const btnHeight = 30;
+        const navHeight = styles.padding * 2 + btnHeight;
 
-        const centerX = width * 0.5;
-        const centerY = height * 0.5;
-
-        const navHeight = height*0.07;
-        const navColor = 0xaf826b;
-        const btnColor = 0xb16551;
-        const btnBorderColor = 0x462820;
-        const btnPadding = height*0.02;
-
-        const buttonHeight = height * 0.05;
-
-        const nav = this.add.rectangle(0,0, width, navHeight, navColor).setOrigin(0,0);
-        const btn = this.add.rectangle(height * 0.01, height * 0.01, 50, buttonHeight, btnColor).setOrigin(0,0);
-        btn.setStrokeStyle(2, btnBorderColor);
+        const nav = this.add.rectangle(0,0, styles.viewPort.width, navHeight, styles.colors.windowBg).setOrigin(0,0);
+        const btn = this.add.rectangle(styles.padding, styles.padding, 50, btnHeight, styles.colors.btnBg).setOrigin(0,0);
+        btn.setStrokeStyle(2, styles.colors.btnBorder);
         const btnBounds = btn.getBounds();
+
         // smth wrong with buttontext it throws err
-        // const btnTxt = this.add.text(btn.x + btnPadding, btnBounds.centerY, 'character').setOrigin(0, 0.5);
-        // btn.setSize(btnTxt.width + btnPadding * 2, btn.height);
+        const btnTxt = this.add.text(btn.x + styles.padding, btnBounds.centerY, 'char', {fontSize: styles.fontSize.large}).setOrigin(0, 0.5);
+        btn.setSize(btnTxt.width + styles.padding * 2, btn.height);
+
         btn.setInteractive();
         btn.on('pointerover', () => {
-            btn.setFillStyle(btnBorderColor);
+            btn.setFillStyle(styles.colors.btnBorder);
         }, this);
         btn.on('pointerout', () => {
-            btn.setFillStyle(btnColor);
+            btn.setFillStyle(styles.colors.btnBg);
         }, this);
         btn.on('pointerdown', () => {
             this.scene.launch(cfg.scenes.character);
