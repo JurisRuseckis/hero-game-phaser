43f217b9636b372521e9781b80534e00704b8588
Juris Ruseckis <juris.ruseckis@catchsmart.com>
2021-05-07 22:48:55 +0300

    add scaling to game

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ import {MainMenuScene} from "./scenes/MainMenuScene";
 import {NavigationScene} from "./scenes/NavigationScene";
 import {LoadingScene} from "./scenes/LoadingScene";
 import {CharacterScene} from "./scenes/CharacterScene";
-
-//TODO: add aspect ratio to properly scale ui
+import {styles} from "./styles";
 
 const config = {
-    type: Phaser.AUTO,
-    parent: 'hero-game',
-    width: window.innerWidth,
-    height: window.innerHeight,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'hero-game',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: styles.viewPort.width,
+        height: styles.viewPort.height
+    },
     scene: [
         MainMenuScene, NavigationScene, LoadingScene, CharacterScene
     ]
