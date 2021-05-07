d9ebb5c9603876f0e69e84a33989abdfc43cb438
Juris Ruseckis <juris.ruseckis@catchsmart.com>
2021-05-07 23:43:54 +0300

    refactor to style js

--- src/scenes/MainMenuScene.js
+++ src/scenes/MainMenuScene.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import {cfg} from "../cfg";
+import {styles} from "../styles";
 
 export class MainMenuScene extends Phaser.Scene
 {
@@ -19,47 +20,41 @@ export class MainMenuScene extends Phaser.Scene
     {
         // will need to load save here
         const menuItems = this.getMenuItems();
-
-        const { width, height } = this.scale;
-
-        const centerX = width * 0.5;
-        const centerY = height * 0.5;
-
-        const menuBoxColor = 0xaf826b;
-        const menuBoxBorderColor = 0x574135;
-        const menuPadding = height * 0.02;
-        const menuWidth = width * 0.33;
-        const menuBorderWidth = menuWidth * 0.006;
-        const menuItemHeight = height * 0.050;
+        const menuItemHeight = 50;
         const menuHeight =
-            menuPadding +
-            height * 0.0666 + // title(.0666 satan)
-            menuPadding +
-            menuItems.length * (menuItemHeight + menuPadding); // btn(.5) + afterpadding(.01)
+            styles.padding * 2 + // padding before & after title
+            styles.defaultTextHeight + // title
+            menuItems.length * (menuItemHeight + styles.padding); // btn + padding between, includes last padding)
 
-        const menuBox = this.add.rectangle(centerX, centerY, menuWidth, menuHeight, menuBoxColor);
-        menuBox.setStrokeStyle(menuBorderWidth, menuBoxBorderColor);
+        const menuBox = this.add.rectangle(styles.viewPort.centerX, styles.viewPort.centerY, styles.grid.window, menuHeight, styles.colors.windowBg);
+        menuBox.setStrokeStyle(styles.borderWidth, styles.colors.windowBorder);
         const menuBoxBounds = menuBox.getBounds();
+        const title = this.add.text(menuBox.x, menuBoxBounds.top + styles.padding, "Hero Game").setOrigin(0.5,0);
 
-        const title = this.add.text(menuBox.x, menuBoxBounds.top + menuPadding, "Hero Game").setOrigin(0.5,0);
-
-        const btnBoxColor = 0xb16551;
-        const btnBoxBorderColor = 0x462820;
         const buttons = menuItems.map((menuItem, index) => {
-            const btn = this.add.rectangle(menuBox.x, menuBoxBounds.top + title.width + menuPadding + index * (menuItemHeight + menuPadding), menuWidth - menuPadding*2, menuItemHeight, btnBoxColor)
-                .setOrigin(0.5, 0.5);
-            btn.setStrokeStyle(menuBorderWidth, btnBoxBorderColor);
+            const btn = this.add.rectangle(
+                menuBox.x,
+                menuBoxBounds.top + title.height + styles.padding * 2 + index * (menuItemHeight + styles.padding),
+                styles.grid.window - styles.padding*2,
+                menuItemHeight,
+                styles.colors.btnBg
+            ).setOrigin(0.5, 0);
 
-            const txt = this.add.text(menuBox.x, btn.y, menuItem.label).setOrigin(0.5);
+            btn.setStrokeStyle(styles.borderWidth, styles.colors.windowBorder);
+            const btnCenter = btn.getCenter();
+            console.log(btnCenter.y);
+            const txt = this.add.text(menuBox.x, btnCenter.y, menuItem.label).setOrigin(0.5);
 
             btn.setInteractive();
             btn.on('pointerover', () => {
-                btn.setFillStyle(btnBoxBorderColor);
+                btn.setFillStyle(styles.colors.btnBorder);
             }, this);
             btn.on('pointerout', () => {
-                btn.setFillStyle(btnBoxColor);
+                btn.setFillStyle(styles.colors.btnBg);
             }, this);
             btn.on('pointerdown', menuItem.onClick);
+
+            return {btn, txt};
         })
 
     }
