7d7c457a52e4642c84b4f7b9700331ab4bb92425
Juris Ruseckis <juris.ruseckis@catchsmart.com>
2021-05-07 23:43:07 +0300

    add more styles

--- src/styles.js
+++ src/styles.js
@@ -4,7 +4,24 @@ const viewPort = {
 }
 
 export const styles = {
-    viewPort,
+    viewPort: {
+        ...viewPort,
+        centerX: viewPort.width/2,
+        centerY: viewPort.height/2,
+    },
+
+    grid: {
+      window: viewPort.width - 20,
+    },
+
+    colors: {
+        windowBg : 0xaf826b,
+        windowBorder: 0x574135,
+        btnBg: 0xb16551,
+        btnBorder:  0x462820
+    },
+
     padding: 15,
     borderWidth: 2,
+    defaultTextHeight: 17,
 };
