946af1231b72e89809453d64980fc9728c6f4bd2
Juris Ruseckis <juris.ruseckis@catchsmart.com>
2021-05-09 23:14:47 +0300

    Increase resolution

--- src/styles.js
+++ src/styles.js
@@ -1,23 +1,23 @@
 const misc = {
-    padding: 10,
-    borderWidth: 2,
+    padding: 20,
+    borderWidth: 4,
 }
 
 const viewPort = {
-    width: 480,
-    height: 800
+    width: 960,
+    height: 1600
 }
 
 const fontSize = {
-    default: 16,
-    title: 40,
-    large: 20,
+    default: 32,
+    title: 80,
+    large: 40,
 }
 
 const panelLayout = {
-    navHeight: 70,
-    contentStart: 70 + misc.padding,
-    contentHeight: viewPort.height - (70 + misc.padding * 2) // nav height and padding before & after
+    navHeight: 140,
+    contentStart: 140 + misc.padding,
+    contentHeight: viewPort.height - (140 + misc.padding * 2) // nav height and padding before & after
 };
 
 export const styles = {
