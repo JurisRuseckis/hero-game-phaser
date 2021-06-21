3c5e09ff0161c561b282ea01527f9e4a6ff07acd
juris <juris.ruseckis@gmail.com>
2021-06-22 01:27:34 +0300

    Add capitalize helper

---
+++ src/helpers/capitalize.js
@@ -0,0 +1,8 @@
+/**
+ * Gets random number that [0;max]
+ * @param {string} string
+ * @return {string}
+ */
+export const capitalize = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
