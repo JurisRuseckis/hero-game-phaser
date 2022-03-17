3b771dead7a28dcde1c9477724e82bfb99e766ea
juris <juris.ruseckis@gmail.com>
2022-03-17 10:32:02 +0200

    Add singleunits to deploy to custom coords

--- src/models/BattleTeam.js
+++ src/models/BattleTeam.js
@@ -7,10 +7,11 @@ export default class BattleTeam
     constructor(props) {
         // divide team's army into squads etc to enable multiple formations and multiple field commanders
         this.formation = props.formation;
+        this.singleUnits = props.singleUnits || [];
 
-        this.characters = this.formation.flat().filter(c => c instanceof Character);
-        this.height = this.formation.length;
-        this.width = Math.max(...this.formation.map(r => r.length));
+        // this.characters = this.formation.flat().filter(c => c instanceof Character);
+        this.height = this.singleUnits.length > 0 ? 0 : this.formation.length;
+        this.width = this.singleUnits.length > 0 ? 0 : Math.max(...this.formation.map(r => r.length));
         /**
          * default direction will always be to right
          * @type {Phaser.Math.Vector2}
@@ -25,6 +26,10 @@ export default class BattleTeam
     rotateFormation(direction){
         if(direction.x + direction.y > 1){
             direction.normalize();
+        }
+
+        if(this.singleUnits.length > 0){
+            return this.formation;
         }
 
         // probably everything will be f-up by js references
