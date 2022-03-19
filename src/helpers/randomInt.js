ffc8050aa4a0849ca3a1af138530745875521604
juris <juris.ruseckis@gmail.com>
2022-03-20 00:25:54 +0200

    update randomInt.js to support min value

--- src/helpers/randomInt.js
+++ src/helpers/randomInt.js
@@ -1,8 +1,11 @@
 /**
  * Gets random number that [0;max]
  * @param {number} max
+ * @param {number} min=0
  * @return {number}
  */
-export const randomInt = (max) => {
-    return Math.floor(Math.random() * max);
+export const randomInt = (max, min = 0) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min);
 }
