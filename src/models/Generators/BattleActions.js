bb6ac360590823221e7fadc3c93d6caf919b5fb9
Juris Ruseckis <jr@longenesis.com>
2022-08-09 22:36:36 +0300

    WIP: New battleActions

--- src/models/Generators/BattleActions.js
+++ src/models/Generators/BattleActions.js
@@ -1,6 +1,6 @@
 import CombatAction, {actionTags} from "../CombatAction";
 import Phaser from "phaser";
-import {tileType} from "../AI/BattleAI";
+import BattleAI, {tileType} from "../AI/BattleAI";
 
 export const combatActions = {
     // add charge so combatant can damage someone with moving
@@ -79,13 +79,27 @@ export const combatActions = {
         cooldown: 0,
         operation: (executor, target, arena) => {
             executor.turnMeter = 0;
+            
+            // calculate
+            const dX = executor.coordinates.x - target.tile.x;
+            const dY = executor.coordinates.y - target.tile.y;
+            // curently max == 2 so no diagonals and only 1 tile to check
+            let middleTile = false;
+            if(Math.abs(dX) > 1){
+                middleTile = arena.tilemap.getTileAt(target.tile.x+dX/2,target.tile.y);
+            } else if (Math.abs(dY) > 1){
+                middleTile = arena.tilemap.getTileAt(target.tile.x,target.tile.y+dY/2);
+            }
+
+            executor.coordinates.set(middleTile.x, middleTile.y);
+            
             const dmg = executor.calculateDmg();
             target.combatant.hp -= dmg;
+
+
             return `${executor.label} from team ${executor.team} charges at ${target.combatant.label} from team ${target.combatant.team} for ${dmg} damage!`;
         },
         targetRules: (executor, target, arena) => {
-            return executor.team !== target.combatant.team;
-
             // calculate
             const dX = executor.coordinates.x - target.tile.x;
             const dY = executor.coordinates.y - target.tile.y;
@@ -100,8 +114,56 @@ export const combatActions = {
             return middleTile
                 && !middleTile.properties['cmbId']
                 && middleTile.index !== tileType.wall
-                && !(middleTile.x === executor.coordinates.x
-                    && middleTile.y === executor.coordinates.y)
+                && executor.team !== target.combatant.team
+        }
+    }),
+    bowAttack: new CombatAction({
+        key: 'bowAttack',
+        tags: [actionTags.targetable],
+        range: 3,
+        cooldown: 0,
+        operation: (executor, target, arena) => {
+            executor.turnMeter = 0;
+            
+            const dmg = executor.calculateDmg();
+            target.combatant.hp -= dmg;
+
+            return `${executor.label} from team ${executor.team} shoots arrow at ${target.combatant.label} from team ${target.combatant.team} for ${dmg} damage!`;
+        },
+        targetRules: (executor, target, arena) => {
+            // calculate
+            const dX = executor.coordinates.x - target.tile.x;
+            const dY = executor.coordinates.y - target.tile.y;
+            // curently max == 2 so no diagonals and only 1 tile to check
+            let middleTile = false;
+            const executorTile = arena.tilemap.getTileAt(executor.coordinates.x,executor.coordinates.y);
+            const distance = BattleAI.getDistanceCost(executorTile,target.tile);
+            
+            let tilesBlocking = true;
+            let middleTiles = [];
+            if(distance > 0 && distance <= 1.4){
+                // when no tiles are between
+                tilesBlocking=true;
+            } else if (distance <= 2){
+                // 1 tile between
+
+            } else if(distance <=3){
+                // 2 tiles between
+                
+            } else {
+                return false;
+            }
+
+            return middleTile.index !== tileType.wall
+                && executor.team !== target.combatant.team
         }
     })
 }
+
+// -,- -,- -,- 3,0 -,- -,- -,-
+// -,- 2,8 2,4 2,0 2,4 2,8 -,-
+// -,- 2,4 1,4 1,0 1,4 2,4 -,-
+// 3,0 2,0 1,0 0,0 1,0 2,0 3,0
+// -,- 2,4 1,4 1,0 1,4 2,4 -,-
+// -,- 2,8 2,4 2,0 2,4 2,8 -,-
+// -,- -,- -,- 3,0 -,- -,- -,-
